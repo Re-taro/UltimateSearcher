6d3c4254894e21ce9062c92528ac7b9c1dfed22b
Rintaro Itokawa <aminokacho@gmail.com>
2021-02-27 00:58:16 +0900

    起動時間の短縮

--- MainActivity.cs
+++ MainActivity.cs
@@ -6,14 +6,13 @@ using Android.Widget;
 using Android.Views;
 using System.Web.NBitcoin;
 using System;
+using System.Threading;
 
 namespace UltimateSearcher
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : AppCompatActivity
     {
-        string result_word;
-
         struct Result
         {
             public string google;
@@ -22,64 +21,14 @@ namespace UltimateSearcher
             public string youtube;
         }
 
-        Result[] result = new Result[10];
-
         enum Searchmode
         {
             google,twitter,qiita,youtube,none
         }
 
+        Result[] result = new Result[10];
         Searchmode searchmode = new Searchmode();
 
-        void resultview()
-        {
-            result_view.Text = result_word;
-            for (int i = 0; i < 10; i++)
-            {
-                result_view.Text += "\n";
-                switch (searchmode)
-                {
-                    case Searchmode.google:
-                        result_view.Text += result[i].google;
-                        break;
-
-                    case Searchmode.twitter:
-                        result_view.Text += result[i].twitter;
-                        break;
-
-                    case Searchmode.qiita:
-                        result_view.Text += result[i].qiita;
-                        break;
-
-                    case Searchmode.youtube:
-                        result_view.Text += result[i].youtube;
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-        }
-
-        void search()
-        {
-            result_word = searchword.Text;
-            string result_wordurl = HttpUtility.UrlEncode(result_word);
-            if (result_word.Length == 0)
-            {
-                button_google.Enabled = false;
-                button_twitter.Enabled = false;
-                button_qiita.Enabled = false;
-                button_youtube.Enabled = false;
-            }
-            else
-            {
-                button_google.Enabled = true;
-                button_twitter.Enabled = true;
-                button_qiita.Enabled = true;
-                button_youtube.Enabled = true;
-            }
-        }
 
         public TextView result_view { get; private set; }
         public EditText searchword { get; private set; }
@@ -91,45 +40,69 @@ namespace UltimateSearcher
         public ImageButton button_search { get; private set; }
 
         protected override void OnCreate(Bundle savedInstanceState)
-        {
-            var systemUiFlags = SystemUiFlags.LayoutStable
-                | SystemUiFlags.LayoutHideNavigation
-                | SystemUiFlags.LayoutFullscreen
-                | SystemUiFlags.HideNavigation
-                | SystemUiFlags.Fullscreen
-                | SystemUiFlags.ImmersiveSticky;
-            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(int)systemUiFlags;
-
-            base.OnCreate(savedInstanceState);
+        {  base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+            Thread thread1 = new Thread(
+                () =>
+                {
+                    result_view = FindViewById<TextView>(Resource.Id.result_view);
 
-            result_view = FindViewById<TextView>(Resource.Id.result_view);
+                    searchword = FindViewById<EditText>(Resource.Id.search_bar);
 
-            searchword = FindViewById<EditText>(Resource.Id.search_bar);
+                    button_google = FindViewById<ImageButton>(Resource.Id.button_google);
+                    button_google.Click += button_google_Click;
 
-            button_google = FindViewById<ImageButton>(Resource.Id.button_google);
-            button_google.Click += button_google_Click;
+                    button_twitter = FindViewById<ImageButton>(Resource.Id.button_twitter);
+                    button_twitter.Click += button_twitter_Click;
+                });
+            thread1.Start();
 
-            button_twitter = FindViewById<ImageButton>(Resource.Id.button_twitter);
-            button_twitter.Click += button_twitter_Click;
+            Thread thread2 = new Thread(
+                () =>
+                {
+                    button_qiita = FindViewById<ImageButton>(Resource.Id.button_qiita);
+                    button_qiita.Click += button_qiita_Click;
 
-            button_qiita = FindViewById<ImageButton>(Resource.Id.button_qiita);
-            button_qiita.Click += button_qiita_Click;
+                    button_youtube = FindViewById<ImageButton>(Resource.Id.button_youtube);
+                    button_youtube.Click += button_youtube_Click;
 
-            button_youtube = FindViewById<ImageButton>(Resource.Id.button_youtube);
-            button_youtube.Click += button_youtube_Click;
-
-            button_search = FindViewById<ImageButton>(Resource.Id.button_search);
-            button_search.Click += button_search_Click;
+                    button_search = FindViewById<ImageButton>(Resource.Id.button_search);
+                    button_search.Click += button_search_Click;
+                });
+            thread2.Start();
 
             for (int i = 0; i < 10; i++)
             {
                 result[i] = new Result();
             }
+        }
 
-            search();
+        void resultview()
+        {
+            result_view.Text = searchword.Text;
+            for (int i = 0; i < 10; i++)
+            {
+                result_view.Text += "\n";
+                switch (searchmode)
+                {
+                    case Searchmode.google:
+                        result_view.Text += result[i].google;
+                        break;
+                    case Searchmode.twitter:
+                        result_view.Text += result[i].twitter;
+                        break;
+                    case Searchmode.qiita:
+                        result_view.Text += result[i].qiita;
+                        break;
+                    case Searchmode.youtube:
+                        result_view.Text += result[i].youtube;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
         private void button_google_Click(object sender, EventArgs e)
@@ -158,7 +131,28 @@ namespace UltimateSearcher
 
         private void button_search_Click(object sender, EventArgs e)
         {
-            search();
+            if (searchword.Text.Length == 0)
+            {
+                button_google.Enabled = false;
+                button_twitter.Enabled = false;
+                button_qiita.Enabled = false;
+                button_youtube.Enabled = false;
+            }
+            else
+            {
+                string result_viewurl = HttpUtility.UrlEncode(searchword.Text);
+                button_google.Enabled = true;
+                button_twitter.Enabled = true;
+                button_qiita.Enabled = true;
+                button_youtube.Enabled = true;
+                for (int i = 0; i < 10; i++)
+                {
+                    result[i].google = "google" + i.ToString();
+                    result[i].twitter = "twitter" + i.ToString();
+                    result[i].qiita = "qiita" + i.ToString();
+                    result[i].youtube = "youtube" + i.ToString();
+                }
+            }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
