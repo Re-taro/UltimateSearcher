204c634407c53386c5c209eb79e5c907b16f8853
Rintaro Itokawa <aminokacho@gmail.com>
2021-03-01 00:44:11 +0900

    Keyの追加

--- Key.cs
+++ Key.cs
@@ -7,4 +7,10 @@
             const string key = "AIzaSyA4J86_jyZI1hncUwzBnafTrZG8hr-GpMc";
             return key;
         }
+        public static string CSE_ID()
+        {
+            const string ID = "b50c20bc1734a5eb0";
+            return ID;
+        }
+    }
 }
