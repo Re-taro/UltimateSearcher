69239384530cacbc2e95977bcef6cbff99b04d96
Rintaro Itokawa <aminokacho@gmail.com>
2021-03-01 00:39:16 +0900

    Keyの追加

---
+++ Key.cs
@@ -0,0 +1,10 @@
+﻿namespace UltimateSearcher
+{
+    public class Keys
+    {
+        public static string Google_API_Key()
+        {
+            const string key = "AIzaSyA4J86_jyZI1hncUwzBnafTrZG8hr-GpMc";
+            return key;
+        }
+}
